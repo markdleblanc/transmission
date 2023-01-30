3e27096979931267d02081889bdb84075dbd4113
Mark LeBlanc <mark@embarkment.co>
2023-01-30 10:49:46 -0700

    Initial email address split
    Ignores quoted @ symbols for split purpose.

--- src/__tests__/address.spec.ts
+++ src/__tests__/address.spec.ts
@@ -1,7 +1,78 @@
 import {describe} from "node:test";
-import {validateEmailAddress} from "../index";
+import {EmailAddress, validateEmailAddress} from "../index";
 
 describe("Email Address Validations", () => {
+
+    test("Email address must contain @", () => {
+        // Arrange
+        /*
+         * Cast to an EmailAddress type to bypass the type check, allowing us
+         * to simulate invalid user input.
+         */
+        const simpleEmail = "a" as EmailAddress;
+
+        // Act
+        const isValid = validateEmailAddress(simpleEmail);
+
+        // Assert
+        expect(isValid).toEqual(false);
+    });
+
+    test("Email address must contain a local part", () => {
+        // Arrange
+        const simpleEmail = "@a";
+
+        // Act
+        const isValid = validateEmailAddress(simpleEmail);
+
+        // Assert
+        expect(isValid).toEqual(false);
+    });
+
+    test("Email address must contain a domain part", () => {
+        // Arrange
+        const simpleEmail = "a@";
+
+        // Act
+        const isValid = validateEmailAddress(simpleEmail);
+
+        // Assert
+        expect(isValid).toEqual(false);
+    });
+
+    test("Email address must not exceed 64 characters", () => {
+        // Arrange
+        const simpleEmail = "a".repeat(64) + "@a" as EmailAddress;
+
+        // Act
+        const isValid = validateEmailAddress(simpleEmail);
+
+        // Assert
+        expect(isValid).toEqual(false);
+    });
+
+    test("Email address with only quoted @ is invalid", () => {
+        // Arrange
+        const simpleEmail = 'user"@"com';
+
+        // Act
+        const isValid = validateEmailAddress(simpleEmail);
+
+        // Assert
+        expect(isValid).toEqual(false);
+    });
+
+    test("Email address with quoted @ is valid", () => {
+        // Arrange
+        const simpleEmail = 'user"@"@com';
+
+        // Act
+        const isValid = validateEmailAddress(simpleEmail);
+
+        // Assert
+        expect(isValid).toEqual(true);
+    });
+
     /**
      * Root TLDs e.g., .com can be used as a domain, therefore an email from
      * a@com is valid. Further, the registration of new TLDs is not restricted, so we
@@ -17,5 +88,4 @@ describe("Email Address Validations", () => {
         // Assert
         expect(isValid).toEqual(true);
     });
-
-})
+});
--- src/index.ts
+++ src/index.ts
@@ -17,5 +17,51 @@ export default function validate(email: EmailAddress) {
 }
 
 export function validateEmailAddress(email: EmailAddress) {
+    if (!email.includes("@")) {
+        return false;
+    }
+
+    const split = (email: string, local?: Local, domain?: Domain, quoted: boolean = false): [string?, string?] => {
+        if (email.length === 0) {
+            return [local, domain];
+        }
+
+        const [head, ...tail] = email;
+
+        if (head === '"') {
+            quoted = !quoted;
+        }
+
+        if (head === "@" && !quoted) {
+            return split("", local, tail.join(""), quoted);
+        }
+
+        return split(tail.join(""), local + head, "", quoted);
+    };
+
+    // Split the email address into local and domain parts using the first @ symbol that isn't enclosed in quotes.
+    const [local, domain] = split(email);
+
+    if (!local || !domain)
+    {
+        return false;
+    }
+
+    if (!validateLocalPart(local)) {
+        return false;
+    }
+
+    return domain.length !== 0;
+}
+
+export function validateLocalPart(local: Local) {
+    if (local.length === 0 || local.length > 64) {
+        return false;
+    }
+
+    if (local.startsWith(".") || local.endsWith(".")) {
+        return false;
+    }
+
     return true;
 }
